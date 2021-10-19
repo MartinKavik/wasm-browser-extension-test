fe8147135434f0945d87e134f4402bc6af309ccd
Martin Kavík <martin@kavik.cz>
2021-10-19 20:43:50 +0200

    add window call

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ use wasm_bindgen::prelude::*;
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
 }
+
+#[wasm_bindgen]
+pub fn window_exists() -> bool {
+    web_sys::window().is_some()
+}
--- worker.js
+++ worker.js
@@ -6,6 +6,7 @@ function handleMessageFromMainThread(event) {
   .then(module => {
     console.log("Invoke 'add' function of WASM module");
     console.log(module.add(41,1));
+    console.log("Window exists:", module.window_exists());
   })
   .catch(console.error);
 }
